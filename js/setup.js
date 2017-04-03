9fae31fe54e0d120f94a275b85b8052d8ae481ce
Vitaliy <katalog.80@mail.ru>
2017-04-03 14:46:44 +0400

    исправление ошибок

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var userSetup = document.querySelector('.setup');
 userSetup.classList.remove('hidden'); // открываем окно установок перед игрой
 // массивы имен и фамилий магов, цвет мантий и глаз
@@ -8,12 +9,20 @@ var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 10
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green']; // цвет глаз
 var similarListElement = userSetup.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.getElementById('similar-wizard-template').content;
+var wizards = toSimilarCharacters(); // список объектов. Содержит сгенерированные рандомно имена с фамилиями, цвет мантии и цвет глаз.
+var fragment = document.createDocumentFragment(); // создаем фрагмент для вставки
 
-var toSimilarCharacters = function () {
+for (var i = 0; i < 5; i++) {
+  fragment.appendChild(renderWizard(wizards[i])); // прогоняем весь массив и вставляем во фрагмент
+}
+similarListElement.appendChild(fragment); // вставляем фрагмент на страницу
+userSetup.querySelector('.setup-similar').classList.remove('hidden');
+
+function toSimilarCharacters() {
   var wizardNames = [];
   var similarCharacters = [];
 
-  for (var i = 0; i < WIZARD_FIRST_NAMES.length; i++) {
+  for (var i = 0; i < 5; i++) {
     var randomFirstName = WIZARD_FIRST_NAMES[Math.floor(Math.random() * (WIZARD_FIRST_NAMES.length))]; // получаем рандом имя
     var randomLastName = WIZARD_LAST_NAMES[Math.floor(Math.random() * (WIZARD_LAST_NAMES.length))]; // получаем рандом фамилию
     var randomCoatColor = WIZARD_COAT_COLOR[Math.floor(Math.random() * (WIZARD_COAT_COLOR.length))]; // получаем рандом цвет мантии
@@ -24,9 +33,7 @@ var toSimilarCharacters = function () {
   return similarCharacters;
 };
 
-var wizards = toSimilarCharacters(); // список объектов. Содержит сгенерированные рандомно имена с фамилиями, цвет мантии и цвет глаз.
-
-var renderWizard = function (wizard) { // в качестве аргумента получаем обьект со именами name coatColor eyesColor
+function renderWizard(wizard) { // в качестве аргумента получаем обьект со именами name coatColor eyesColor
   var wizardElement = similarWizardTemplate.cloneNode(true); // копируем структуру шаблона со всеми потомками .similar-wizard-template
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name; // записываем имя персонажа
@@ -35,10 +42,3 @@ var renderWizard = function (wizard) { // в качестве аргумента
 
   return wizardElement;
 };
-
-var fragment = document.createDocumentFragment(); // создаем фрагмент для вставки
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i])); // прогоняем весь массив и вставляем во фрагмент
-}
-similarListElement.appendChild(fragment); // вставляем фрагмент на страницу
-userSetup.querySelector('.setup-similar').classList.remove('hidden');
